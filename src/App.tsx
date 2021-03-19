45f50818b64418d1d7e436051c394300ba4653d3
山崎大輔 <yamazakidaisuke@yamazakidaisukenoMacBook-Air.local>
2021-03-19 10:31:26 +0900

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 
 import Home from 'containers/pages/Home';
-import Members from 'containers/pages/Members';
 import Youtube from 'containers/pages/Youyube';
 
 import './App.css';
@@ -12,7 +11,6 @@ const App: FC = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/you" element={<Youtube />} />
-      <Route path="/:orgCode/members" element={<Members />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </>
