d2c289d29d8c4311834a83df8af5fe2e4160b670
山崎大輔 <yamazakidaisuke@yamazakidaisukenoMacBook-Air.local>
2021-03-21 13:54:33 +0900

    modify

--- src/containers/pages/Home.tsx
+++ src/containers/pages/Home.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
 
 import Home from 'components/pages/Home';
-import orgCodes from 'data/org-codes';
 
-const EnhancedHome: FC = () => <Home orgCodes={orgCodes} />;
+const EnhancedHome: FC = () => <Home />;
 
 export default EnhancedHome;
--- src/domains/youtube/services/youtube-search.ts
+++ src/domains/youtube/services/youtube-search.ts
@@ -7,6 +7,8 @@ const YoutubeSearch = async (): Promise<Video[]> => {
   const videos = await axios.get<AxiosResponse<any>>(
     `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=citypop&maxResults=3&key=${YOUTUBE_API_KEY}`,
   );
+  // const videos = (await response.json()) as unknown[];
+  console.log(videos);
   const videoData: Video = {
     etag: '',
     videoId: '',
@@ -16,8 +18,6 @@ const YoutubeSearch = async (): Promise<Video[]> => {
     width: 0,
     height: 0,
   };
-  // const videos = (await response.json()) as unknown[];
-  console.log(videos);
 
   if (!isData(videos.data)) {
     throw Error('API type error');
@@ -25,22 +25,17 @@ const YoutubeSearch = async (): Promise<Video[]> => {
   const yData: Data = videos.data;
   console.log(yData);
 
-  const videosData: Video[] = await Promise.all(
-    yData.items.map((movie: item) => {
-      videoData.etag = movie.etag;
-      videoData.videoId = movie.id.videoId;
-      videoData.title = movie.snippet.title;
-      videoData.description = movie.snippet.description;
-      videoData.thumbnails = movie.snippet.thumbnails.medium.url;
-      videoData.width = movie.snippet.thumbnails.medium.width;
-      videoData.height = movie.snippet.thumbnails.medium.height;
-      console.log(videoData);
+  const videosData: Video[] = yData.items.map((movie: item) => {
+    videoData.etag = movie.etag;
+    videoData.videoId = movie.id.videoId;
+    videoData.title = movie.snippet.title;
+    videoData.description = movie.snippet.description;
+    videoData.thumbnails = movie.snippet.thumbnails.medium.url;
+    videoData.width = movie.snippet.thumbnails.medium.width;
+    videoData.height = movie.snippet.thumbnails.medium.height;
 
-      return videoData;
-    }),
-  );
-
-  console.log(videosData);
+    return videoData;
+  });
 
   return videosData;
 };
